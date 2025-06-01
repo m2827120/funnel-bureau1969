6033ac725de7e146bd8b9a4be850997ca9cfe991
Максим Сколубович <m2827120@gmail.com>
2025-06-01 21:22:21 +0500

    Updates

---
+++ static/js/952.54ea2cbc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfunnel_calc=self.webpackChunkfunnel_calc||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.54ea2cbc.chunk.js.map
